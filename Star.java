f75e4199972bd20d9b64d613029aaa79cb775093
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-14 12:48:44 +0530

    Changes According to test cases

--- Star.java
+++ Star.java
@@ -1,31 +1,39 @@
 public class Star {
-            private static final int PATTERN_HEIGHT = 5; // height of Star Pattern
+
+    // Height of the hollow diamond pattern
+    private static final int PATTERN_HEIGHT = 5;
 
     public static void main(String[] args) {
-for (int i = 1; i <= PATTERN_HEIGHT; i++) {
+
+        // upper half
+        for (int i = 1; i <= PATTERN_HEIGHT; i++) {
             printRow(i);
         }
 
+        // lower half
         for (int i = PATTERN_HEIGHT - 1; i >= 1; i--) {
             printRow(i);
         }
     }
- 
-        private static void printRow(int i) {
 
-        for (int s = 1; s <= PATTERN_HEIGHT - i; s++) {
+    // prints a single row of the hollow diamond
+    private static void printRow(int row) {
+
+        // leading spaces to center the pattern
+        for (int s = 1; s <= PATTERN_HEIGHT - row; s++) {
             System.out.print(" ");
         }
 
         System.out.print("*");
 
-        if (i > 1) {
-            for (int sp = 1; sp <= (2 * i - 3); sp++) {
+        // inner hollow spaces
+        if (row > 1) {
+            for (int sp = 1; sp <= (2 * row - 3); sp++) {
                 System.out.print(" ");
             }
             System.out.print("*");
         }
-    
+
         System.out.println();
     }
 }
