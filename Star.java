05874090febcf6421f0c8628f897fe0a15e26b05
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-14 12:43:15 +0530

    Created the method to print the row

--- Star.java
+++ Star.java
@@ -3,46 +3,22 @@ public class Star {
 
     public static void main(String[] args) {
 
+ 
+        private static void printRow(int i) {
 
-        // Upper part of Star Pattern
-        for (int i = 1; i <= PATTERN_HEIGHT; i++) {
-
-            
-            // print spaces to center the stars
-
-            for (int s = 1; s <= PATTERN_HEIGHT - i; s++) {
-                System.out.print(" ");
-            }
-
-            System.out.print("*"); // first star
-
-            if (i > 1) {
-                for (int sp = 1; sp <= (2 * i - 3); sp++) {
-                    System.out.print(" ");
-                }
-                System.out.print("*");
-            }
-
-            System.out.println();
+        for (int s = 1; s <= PATTERN_HEIGHT - i; s++) {
+            System.out.print(" ");
         }
 
-        // Lower half of Star Pattern
-        for (int i = PATTERN_HEIGHT - 1; i >= 1; i--) {
+        System.out.print("*");
 
-            for (int s = 1; s <= PATTERN_HEIGHT - i; s++) {
+        if (i > 1) {
+            for (int sp = 1; sp <= (2 * i - 3); sp++) {
                 System.out.print(" ");
             }
-
             System.out.print("*");
-
-            if (i > 1) {
-                for (int sp = 1; sp <= (2 * i - 3); sp++) {
-                    System.out.print(" ");
-                }
-                System.out.print("*");
-            }
-
-            System.out.println(); // next row
         }
+    
+        System.out.println();
     }
 }
