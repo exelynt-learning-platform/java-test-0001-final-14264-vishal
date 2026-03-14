7188af7099be7232a5c8bece8274e2004838a41d
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-14 12:19:45 +0530

    Added the spaces print statement uses in lower part

--- Star.java
+++ Star.java
@@ -1,17 +1,29 @@
 public class Star {
     public static void main(String[] args) {
-        int n=5;
-        
-        for (int i=1; i<=n; i++){
-            for(int s=1; s<=n-i; s++){
+
+        int n = 5;
+
+        // upper part
+        for (int i = 1; i <= n; i++) {
+
+            for (int s = 1; s <= n - i; s++) {
                 System.out.print(" ");
             }
-            System.out.println("*");
-        
-                    System.out.println();
 
-}
-     for (int i = n - 1; i >= 1; i--) {
+            System.out.print("*");
+
+            if (i > 1) {
+                for (int sp = 1; sp <= (2 * i - 3); sp++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // lower part
+        for (int i = n - 1; i >= 1; i--) {
 
             for (int s = 1; s <= n - i; s++) {
                 System.out.print(" ");
