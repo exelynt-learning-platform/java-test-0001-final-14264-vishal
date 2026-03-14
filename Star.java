91d10d6364c200b5cfe55d30d17c243e3559b99f
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-14 12:13:30 +0530

    Added the lower part of pattern

--- Star.java
+++ Star.java
@@ -7,6 +7,26 @@ public class Star {
                 System.out.print(" ");
             }
             System.out.println("*");
+        
+                    System.out.println();
+
+}
+     for (int i = n - 1; i >= 1; i--) {
+
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            System.out.print("*");
+
+            if (i > 1) {
+                for (int sp = 1; sp <= (2 * i - 3); sp++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+
+            System.out.println();
         }
     }
 }
