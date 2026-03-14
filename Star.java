24a68207313727edcef0ad0071772afd176b6bcb
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-14 12:08:10 +0530

    Created the initial space before print *

--- Star.java
+++ Star.java
@@ -1,5 +1,11 @@
 public class Star {
     public static void main(String[] args) {
+        int n=5;
         
+        for (int i=1; i<=n; i++){
+            for(int s=1; s<=n-i; s++){
+                System.out.print(" ");
+            }
+        }
     }
 }
